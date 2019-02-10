6f170eacdd6afd1fecb7322157eb7a4370756fc1
rthowerton <howertor@oregonstate.edu>
2019-02-09 17:33:04 -0800

    Tentatively found the error.

--- dev/install.sh
+++ dev/install.sh
@@ -9,7 +9,7 @@ if [[ $cont =~ ^([yY][eE][sS]|[yY])$ ]]; then
    echo "================ Installing Necessary Packages =================="
    echo "================================================================="
    # Start with necessary packages
-   apt-get install libtool pkg-config build-essential autoconf automake git
+   sudo apt-get install libtool pkg-config build-essential autoconf automake git
 
    # Download ZMQ package
    echo "================================================================="
@@ -20,13 +20,13 @@ if [[ $cont =~ ^([yY][eE][sS]|[yY])$ ]]; then
    cd zeromq-4.1.4/
    ./configure --without-libsodium
    make
-   make install
-   ldconfig
+   sudo make install
+   sudo ldconfig
 
    # Get ZMQ CPP files
    cd /home/pi
    git clone https://github.com/zeromq/cppzmq.git
-   cp cppzmq/zmq.hpp /usr/local/include/
+   sudo cp cppzmq/zmq.hpp /usr/local/include/
 
    # Get and compile brypt-node
    echo "================================================================="
@@ -43,22 +43,22 @@ if [[ $cont =~ ^([yY][eE][sS]|[yY])$ ]]; then
    # Run DNSMasq and HostAPD
    read -r -p "The script will now install packages necessary for instantiating a Wi-Fi access point/hotspot. This will cause your device to restart. Do you wish to continue? [y/N]" cont
    if [[ $cont =~ ^([yY][eE][sS]|[yY])$ ]]; then
-      apt-get install dnsmasq hostapd
-      systemctl stop dnsmasq
-      systemctl stop hostapd
+      sudo apt-get install dnsmasq hostapd
+      sudo systemctl stop dnsmasq
+      sudo systemctl stop hostapd
    fi
 
    read -r -p "Do you wish to set up a wireless access point after reboot? [y/N]" cont
    if [[ $cont =~ ^([yY][eE][sS]|[yY])$ ]]; then
-      touch /root/start_ap
+      sudo touch /root/start_ap
    fi
 fi
 
 # Prepare the system for reboot and starting the access point
-cp /etc/rc.local /home/pi/brypt-node/dev/config/AP/rc.local.base
-cp /home/pi/brypt-node/dev/config/AP/rc.local.reboot /etc/rc.local
-chmod +x /etc/rc.local
+sudo cp /etc/rc.local /home/pi/brypt-node/dev/config/AP/rc.local.base
+sudo cp /home/pi/brypt-node/dev/config/AP/rc.local.reboot /etc/rc.local
+sudo chmod +x /etc/rc.local
 
 echo "The system will now restart in 10 seconds. Press Ctrl-C to stop reboot."
 sleep 10
-reboot
+sudo reboot
